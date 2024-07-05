c70de81d3b92c2f8b9109b03ca6cd15e66c08031
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-05 10:59:30 +0300

    do linear search

--- kata-machine/src/day1/LinearSearchList.ts
+++ kata-machine/src/day1/LinearSearchList.ts
@@ -1,3 +1,17 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    let result = false;
 
+    for (let i = 0; i < haystack.length; i++) {
+        const element = haystack[i];
+
+        if (element === needle) {
+            result = true;
+            return result;
+        }
+    }
+
+    return result;
 }
