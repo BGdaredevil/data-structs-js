394734e9f84d8741fb393e1023a49cd19b3d0d46
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-05 12:06:40 +0300

    do two crystal balls

--- kata-machine/src/day1/TwoCrystalBalls.ts
+++ kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jump = Math.sqrt(breaks.length);
+    let lo = 0;
 
+    while (lo < breaks.length) {
+        const currIdx = Math.floor(lo + jump);
+
+        if (breaks[currIdx]) {
+            for (let i = lo; i < currIdx; i++) {
+                const element = breaks[i];
+
+                if (element) {
+                    return i;
+                }
+            }
+        }
+
+        lo = currIdx;
+    }
+
+    return -1;
 }
