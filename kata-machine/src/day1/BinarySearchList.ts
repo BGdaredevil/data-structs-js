5c6248b548e08f65f5d8b54ab4177b6df3fdb6cd
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-05 11:42:15 +0300

    do binary search non recursive

--- kata-machine/src/day1/BinarySearchList.ts
+++ kata-machine/src/day1/BinarySearchList.ts
@@ -1,19 +1,45 @@
+// recursive
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//     if (haystack.length <= 1) {
+//         return haystack[0] === needle;
+//     }
+
+//     const median = Math.floor(haystack.length / 2);
+
+//     const item = haystack[median];
+
+//     if (item === needle) {
+//         return true;
+//     }
+
+//     if (item < needle) {
+//         return bs_list(haystack.slice(median + 1), needle);
+//     }
+
+//     return bs_list(haystack.slice(0, median), needle);
+// }
+
+// non recursive
+
 export default function bs_list(haystack: number[], needle: number): boolean {
-    if (haystack.length <= 1) {
-        return haystack[0] === needle;
+    let hiIndex = haystack.length;
+    let loIndex = 0;
+
+    while (loIndex < hiIndex) {
+        const median = Math.floor(loIndex + (hiIndex - loIndex) / 2);
+        const value = haystack[median];
+
+        if (value === needle) {
+            return true;
+        }
+
+        if (value < needle) {
+            loIndex = median + 1;
+            continue;
+        }
+
+        hiIndex = median;
     }
 
-    const median = Math.floor(haystack.length / 2);
-
-    const item = haystack[median];
-
-    if (item === needle) {
-        return true;
-    }
-
-    if (item < needle) {
-        return bs_list(haystack.slice(median + 1), needle);
-    }
-
-    return bs_list(haystack.slice(0, median), needle);
+    return false;
 }
