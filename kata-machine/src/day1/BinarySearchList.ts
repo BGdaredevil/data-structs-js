8988ecc85845893831287aafeb20ffbfe575617c
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-05 11:33:03 +0300

    do binary search

--- kata-machine/src/day1/BinarySearchList.ts
+++ kata-machine/src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    if (haystack.length <= 1) {
+        return haystack[0] === needle;
+    }
 
+    const median = Math.floor(haystack.length / 2);
+
+    const item = haystack[median];
+
+    if (item === needle) {
+        return true;
+    }
+
+    if (item < needle) {
+        return bs_list(haystack.slice(median + 1), needle);
+    }
+
+    return bs_list(haystack.slice(0, median), needle);
 }
