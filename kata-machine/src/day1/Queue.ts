8ed89413c637e351764791d4357f3f49c58c4fb9
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-08 11:55:26 +0300

    do que

--- kata-machine/src/day1/Queue.ts
+++ kata-machine/src/day1/Queue.ts
@@ -1,18 +1,54 @@
+class Node<U> {
+    public value: U;
+    public next?: Node<U>;
+    // public prev?: Node<U>;
+
+    constructor(val: U) {
+        this.value = val;
+        this.next = undefined;
+        // this.prev = undefined;
+    }
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const nodeItem = new Node(item);
 
-}
+        if (this.length === 0) {
+            this.head = this.tail = nodeItem;
+            this.length++;
+
+            return;
+        }
+
+        this.tail!.next = nodeItem;
+        this.tail = nodeItem;
+        this.length++;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return;
+        }
 
-}
+        const firstItem = this.head;
+
+        this.head = this.head?.next;
+        this.length--;
+
+        return firstItem?.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
