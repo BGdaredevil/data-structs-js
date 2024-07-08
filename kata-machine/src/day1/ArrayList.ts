a7c1a158c90d9bc95cdf6a15adaddde70049d1a2
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-08 13:29:14 +0300

    implement array list

--- kata-machine/src/day1/ArrayList.ts
+++ kata-machine/src/day1/ArrayList.ts
@@ -1,27 +1,108 @@
 export default class ArrayList<T> {
     public length: number;
+    private store: (T | undefined)[];
+    private capacity: number;
 
-    
-
-    constructor() {
+    constructor(capacity: number) {
+        this.store = new Array(capacity);
+        this.capacity = capacity;
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        this.insertAt(item, 0);
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        while (idx >= this.capacity) {
+            this.grow();
+        }
 
-}
+        if (this.length + 1 > this.capacity) {
+            this.grow();
+        }
+
+        let tmp = this.store[idx];
+        this.store[idx] = item;
+
+        for (let i = idx + 1; i < this.capacity; i++) {
+            let curr = this.store[i];
+            this.store[i] = tmp;
+            tmp = curr;
+        }
+
+        this.length++;
+    }
+
     append(item: T): void {
+        if (this.capacity == this.length) {
+            this.grow();
+        }
 
-}
-    remove(item: T): T | undefined {
+        this.store[this.length] = item;
+        this.length++;
+    }
 
-}
-    get(idx: number): T | undefined {
-
-}
     removeAt(idx: number): T | undefined {
+        if (idx >= this.length) {
+            return;
+        }
 
-}
+        const foundItem = this.store[idx];
+
+        for (let i = idx; i < this.length - 1; i++) {
+            this.store[i] = this.store[i + 1];
+        }
+
+        this.length--;
+        this.store[this.length] = undefined;
+
+        return foundItem;
+    }
+
+    remove(item: T): T | undefined {
+        let foundItem = undefined;
+        let foundIdx = 0;
+
+        for (let i = 0; i < this.length; i++) {
+            if (this.store[i] === item) {
+                foundItem = this.store[i];
+                foundIdx = i;
+
+                break;
+            }
+        }
+
+        if (!foundItem) {
+            return;
+        }
+
+        for (let i = foundIdx; i < this.length - 1; i++) {
+            this.store[i] = this.store[i + 1];
+        }
+
+        this.length--;
+        this.store[this.length] = undefined;
+
+        return foundItem;
+    }
+
+    get(idx: number): T | undefined {
+        if (idx >= this.length) {
+            return;
+        }
+
+        return this.store[idx];
+    }
+
+    private grow(): void {
+        this.capacity = this.capacity * 2;
+        const newStore = new Array(this.capacity);
+
+        for (let i = 0; i < this.store.length; i++) {
+            newStore[i] = this.store[i];
+        }
+
+        this.store = newStore;
+    }
 }
