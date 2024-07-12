fdab7883f518a145564edd0d49c0ab58bd2ca203
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-12 15:19:33 +0300

    do breadth first search on binary tree

--- kata-machine/src/day1/BTBFS.ts
+++ kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,39 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const que = new Queue<BinaryNode<number>>();
+
+    return traverse(head, que, needle);
+}
+
+function traverse(node: BinaryNode<number> | undefined, nodesToDo: Queue<BinaryNode<number>>, toFind: number): boolean {
+    if (!node) {
+        return false;
+    }
+
+    const found = visit(node, nodesToDo, toFind);
+    
+    if (found) {
+        return true;
+    }
+
+    return traverse(nodesToDo.deque(), nodesToDo, toFind);
+}
+
+function visit(node: BinaryNode<number>, nodesToDo: Queue<BinaryNode<number>>, itemToFind: number): boolean {
+    const found = node.value === itemToFind;
+
+    if (found) {
+        return found;
+    }
+
+    if (node.left) {
+        nodesToDo.enqueue(node.left);
+    }
+
+    if (node.right) {
+        nodesToDo.enqueue(node.right);
+    }
+
+    return found;
 }
