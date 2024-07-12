642bc285859e843c900dd7e74c21bfec738132c1
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-12 15:28:31 +0300

    do breadth first search on binary tree iterative

--- kata-machine/src/day1/BTBFS.ts
+++ kata-machine/src/day1/BTBFS.ts
@@ -1,39 +1,62 @@
 import Queue from "./Queue";
 
+// recursive
+// export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+//     const que = new Queue<BinaryNode<number>>();
+
+//     return traverse(head, que, needle);
+// }
+
+// function traverse(node: BinaryNode<number> | undefined, nodesToDo: Queue<BinaryNode<number>>, toFind: number): boolean {
+//     if (!node) {
+//         return false;
+//     }
+
+//     const found = visit(node, nodesToDo, toFind);
+
+//     if (found) {
+//         return true;
+//     }
+
+//     return traverse(nodesToDo.deque(), nodesToDo, toFind);
+// }
+
+// function visit(node: BinaryNode<number>, nodesToDo: Queue<BinaryNode<number>>, itemToFind: number): boolean {
+//     const found = node.value === itemToFind;
+
+//     if (found) {
+//         return found;
+//     }
+
+//     if (node.left) {
+//         nodesToDo.enqueue(node.left);
+//     }
+
+//     if (node.right) {
+//         nodesToDo.enqueue(node.right);
+//     }
+
+//     return found;
+// }
+
+// iterative
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-    const que = new Queue<BinaryNode<number>>();
+    const que = new Queue<BinaryNode<number> | null>();
+    que.enqueue(head);
 
-    return traverse(head, que, needle);
-}
+    while (que.length) {
+        const curr = que.deque();
+        if (!curr) {
+            continue;
+        }
 
-function traverse(node: BinaryNode<number> | undefined, nodesToDo: Queue<BinaryNode<number>>, toFind: number): boolean {
-    if (!node) {
-        return false;
+        if (curr.value === needle) {
+            return true;
+        }
+
+        que.enqueue(curr.left);
+        que.enqueue(curr.right);
     }
 
-    const found = visit(node, nodesToDo, toFind);
-    
-    if (found) {
-        return true;
-    }
-
-    return traverse(nodesToDo.deque(), nodesToDo, toFind);
-}
-
-function visit(node: BinaryNode<number>, nodesToDo: Queue<BinaryNode<number>>, itemToFind: number): boolean {
-    const found = node.value === itemToFind;
-
-    if (found) {
-        return found;
-    }
-
-    if (node.left) {
-        nodesToDo.enqueue(node.left);
-    }
-
-    if (node.right) {
-        nodesToDo.enqueue(node.right);
-    }
-
-    return found;
+    return false;
 }
