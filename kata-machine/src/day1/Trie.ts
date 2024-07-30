f84a671a88e72f0a0a041ee7ab7d3029554456ad
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-07-30 12:15:37 +0300

    solve trie with a parrent ref

--- kata-machine/src/day1/Trie.ts
+++ kata-machine/src/day1/Trie.ts
@@ -1,14 +1,28 @@
-// personal implementation
+// internet inspired
+
 class Node<U> {
     public value: U;
+    public parrent: Node<U> | null;
     public children: (Node<U> | null)[];
     public isWord: boolean;
-    // public prev?: Node<U>;
+
+    public getWord() {
+        let result = "";
+        let current: Node<U> | null = this;
+
+        while (current !== null) {
+            result = current.value + result;
+            current = current.parrent;
+        }
+
+        return result;
+    }
 
     constructor(val: U) {
         this.value = val;
         this.children = new Array(26).fill(null);
         this.isWord = false;
+        this.parrent = null;
     }
 }
 
@@ -28,7 +42,9 @@ export default class Trie {
             const charIdx = this.getIdx(char);
 
             if (!current!.children[charIdx]) {
-                current!.children[charIdx] = new Node(char);
+                const nodeToInsert = new Node(char);
+                nodeToInsert.parrent = current;
+                current!.children[charIdx] = nodeToInsert;
             }
 
             current = current!.children[charIdx];
@@ -40,46 +56,58 @@ export default class Trie {
     }
 
     delete(item: string): void {
-        let current = this.head;
+        let current: Node<string> | null = this.head;
 
         if (!current) {
             return;
         }
 
-        let nodes: Node<string>[] = [];
-
         for (let i = 0; i < item.length; i++) {
-            const char = item[i];
-            const charIdx = this.getIdx(char);
-
-            if (!current!.children[charIdx]) {
+            if (!current) {
                 return;
             }
 
-            nodes.push(current!);
-            current = current!.children[charIdx];
+            const char = item[i];
+            const charIdx = this.getIdx(char);
+            const nextNode: any = current.children[charIdx];
+
+            if (!nextNode) {
+                return;
+            }
+
+            current = nextNode as Node<string>;
         }
 
-        if (current!.isWord) {
-            current!.isWord = false;
+        if (current.isWord) {
+            current.isWord = false;
         }
 
-        if (current!.children.some((e) => e !== null)) {
-            return;
-        }
+        for (let i = item.length; i <= 0; i--) {
+            const char = item[i];
+            const charIdx = this.getIdx(char);
 
-        for (let i = nodes.length; i >= 0; i--) {
-            const currNode = nodes[i];
-            const currChar = item[i];
-            const itemidx = this.getIdx(currChar);
-            const node = currNode.children[itemidx];
+            if (!current.parrent) {
+                return;
+            }
 
-            if (node?.children.some((e) => e !== null)) {
+            // const prevNode = current.parrent.children[charIdx];
+            // prevNode!.children[charIdx] = null;
+
+            if (current.children.some((e) => e !== null)) {
                 continue;
             } else {
-                currNode.children[itemidx] = null;
+                current.parrent.children[charIdx] = null;
             }
+
+            current = current.parrent;
         }
+
+        // while (current !== null) {
+        //     if (current.children.some((e) => e !== null)) {
+        //         return;
+        //     }
+
+        // }
     }
 
     find(partial: string): string[] {
@@ -102,35 +130,21 @@ export default class Trie {
             return result;
         }
 
-        const items = this.traverse(current, []);
-        // hack
-
-        const ss = partial.split("");
-        ss.pop();
-        const tt = ss.join("");
-
-        // result.push(...items.map((e) => partial + e));
-        result.push(...items.map((e) => tt + e));
+        this.getWords(current, result);
 
         return result;
     }
 
-    private traverse(node: Node<string>, results: string[]) {
+    private getWords(node: Node<string>, results: string[]): string[] {
+        if (node.isWord) {
+            results.push(node.getWord());
+        }
+
         for (let i = 0; i < node.children.length; i++) {
             const nextNode = node.children[i];
-
             if (nextNode) {
-                // results.push(...this.traverse(nextNode, []));
-                this.traverse(nextNode, results);
+                this.getWords(nextNode, results);
             }
-        }
-
-        for (let i = 0; i < results.length; i++) {
-            results[i] = node.value + results[i];
-        }
-
-        if (node.isWord) {
-            results.push(node.value);
         }
 
         return results;
@@ -141,6 +155,150 @@ export default class Trie {
     }
 }
 
+// // personal implementation
+// class Node<U> {
+//     public value: U;
+//     public children: (Node<U> | null)[];
+//     public isWord: boolean;
+//     // public prev?: Node<U>;
+
+//     constructor(val: U) {
+//         this.value = val;
+//         this.children = new Array(26).fill(null);
+//         this.isWord = false;
+//     }
+// }
+
+// export default class Trie {
+//     private normConstant = "a".charCodeAt(0);
+//     private head: Node<string> | null;
+
+//     constructor() {
+//         this.head = new Node("");
+//     }
+
+//     insert(item: string): void {
+//         let current = this.head;
+
+//         for (let i = 0; i < item.length; i++) {
+//             const char = item[i];
+//             const charIdx = this.getIdx(char);
+
+//             if (!current!.children[charIdx]) {
+//                 current!.children[charIdx] = new Node(char);
+//             }
+
+//             current = current!.children[charIdx];
+
+//             if (i === item.length - 1) {
+//                 current!.isWord = true;
+//             }
+//         }
+//     }
+
+//     delete(item: string): void {
+//         let current = this.head;
+
+//         if (!current) {
+//             return;
+//         }
+
+//         let nodes: Node<string>[] = [];
+
+//         for (let i = 0; i < item.length; i++) {
+//             const char = item[i];
+//             const charIdx = this.getIdx(char);
+
+//             if (!current!.children[charIdx]) {
+//                 return;
+//             }
+
+//             nodes.push(current!);
+//             current = current!.children[charIdx];
+//         }
+
+//         if (current!.isWord) {
+//             current!.isWord = false;
+//         }
+
+//         if (current!.children.some((e) => e !== null)) {
+//             return;
+//         }
+
+//         for (let i = nodes.length; i >= 0; i--) {
+//             const currNode = nodes[i];
+//             const currChar = item[i];
+//             const itemidx = this.getIdx(currChar);
+//             const node = currNode.children[itemidx];
+
+//             if (node?.children.some((e) => e !== null)) {
+//                 continue;
+//             } else {
+//                 currNode.children[itemidx] = null;
+//             }
+//         }
+//     }
+
+//     find(partial: string): string[] {
+//         const result: string[] = [];
+//         let current = this.head;
+
+//         for (let i = 0; i < partial.length; i++) {
+//             const char = partial[i];
+//             const charIdx = this.getIdx(char);
+//             const nextNode = current?.children[charIdx];
+
+//             if (!nextNode) {
+//                 return result;
+//             }
+
+//             current = nextNode;
+//         }
+
+//         if (!current) {
+//             return result;
+//         }
+
+//         const items = this.traverse(current, []);
+//         // hack
+
+//         const ss = partial.split("");
+//         ss.pop();
+//         const tt = ss.join("");
+
+//         // result.push(...items.map((e) => partial + e));
+//         result.push(...items.map((e) => tt + e));
+
+//         return result;
+//     }
+
+//     private traverse(node: Node<string>, results: string[]) {
+//         for (let i = 0; i < node.children.length; i++) {
+//             const nextNode = node.children[i];
+
+//             if (nextNode) {
+//                 // results.push(...this.traverse(nextNode, []));
+//                 this.traverse(nextNode, results);
+//             }
+//         }
+
+//         for (let i = 0; i < results.length; i++) {
+//             results[i] = node.value + results[i];
+//         }
+
+//         if (node.isWord) {
+//             results.push(node.value);
+//         }
+
+//         return results;
+//     }
+
+//     private getIdx(str: string) {
+//         return str.charCodeAt(0) - this.normConstant;
+//     }
+// }
+
+// testing implementation
 // const trie = new Trie();
 // trie.insert("foo");
 // trie.insert("fool");
