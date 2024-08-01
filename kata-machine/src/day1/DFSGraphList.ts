810279fdc9c6c8053c228cb31483c0514a57b91c
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-08-01 13:06:10 +0300

    implement dfs on a graph list

--- kata-machine/src/day1/DFSGraphList.ts
+++ kata-machine/src/day1/DFSGraphList.ts
@@ -1,3 +1,42 @@
 export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
 
+    const result = traverse(graph, source, needle, seen, path);
+
+    return result ? path : null;
+}
+
+function traverse(
+    graph: WeightedAdjacencyList,
+    current: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (seen[current]) {
+        return false;
+    }
+
+    seen[current] = true;
+
+    path.push(current);
+
+    if (current === needle) {
+        return true;
+    }
+
+    const edges = graph[current];
+
+    for (let i = 0; i < edges.length; i++) {
+        const edge = edges[i];
+
+        if (traverse(graph, edge.to, needle, seen, path)) {
+            // path.push(edge.to)
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
 }
